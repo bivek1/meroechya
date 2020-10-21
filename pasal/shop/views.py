d9742003ff737ec918d77eafc5b333ce9f2de707
bivek1 <bivekdhakal18@gmail.com>
2020-10-21 20:37:03 +0545

    fifth

--- pasal/shop/views.py
+++ pasal/shop/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def homepage(request):
-    return HttpResponse("<h1 align='text-center'>Grand Technology Solution PVT LTD WEBAPP Update will be available here until we hosted it.</h1>")
+    return render(request, 'base.html')
+    # return HttpResponse("<h1 align='text-center'>Grand Technology Solution PVT LTD WEBAPP Update will be available here until we hosted it.</h1>")
